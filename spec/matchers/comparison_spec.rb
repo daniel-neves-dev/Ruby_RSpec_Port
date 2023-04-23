aa3681fe4e9a2bc68d4b847f28981a68b3deb3c9
daniel <daniel.oliveira.au@hotmail.com>
2023-04-23 14:54:55 -0300

    test: eq matcher

--- spec/matchers/comparison_spec.rb
+++ spec/matchers/comparison_spec.rb
@@ -1,8 +1,14 @@
-RSpec.describe "Comparison 'equal'" do
-  it "Same object" do
+RSpec.describe "Comparison" do
+  it "#equal or #be - same object" do
     x = "ruby"
     y = "ruby"
     expect(x).not_to equal(y)
     expect(x).to equal(x)
   end
+
+  it "#eql or #eq - same valor" do
+    x = "ruby"
+    y = "ruby"
+    expect(x).to eq(y)
+    end
 end
