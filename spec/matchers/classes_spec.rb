8c5fa2ef6fa8e6b8cc26d6bf8893437d73009ecd
daniel <daniel.oliveira.au@hotmail.com>
2023-04-23 15:40:28 -0300

    test: classes matchers

--- spec/matchers/classes_spec.rb
+++ spec/matchers/classes_spec.rb
@@ -2,4 +2,7 @@ RSpec.describe "Classes" do
   it "be instance of - exactly to class" do
     expect(10).to be_instance_of(Integer)
   end
+  it "be kind of - inheritance of a class" do
+    expect(10).to be_kind_of(Integer)
+  end
 end
