69382e294088a29fd833fe0eb3acabdf00252c10
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-14 15:51:07 -0300

    text: raise_error

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe Calculator do
     result = subject.sum(-5, -2)
     expect(result).to eq(-7)
   end
+
+  context "#div" do
+    it "divide by 0" do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError,"divided by 0")
+    end
+  end
 end
